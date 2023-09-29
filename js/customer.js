7d62a1f3d8b4a37e834a770f7dd216b6a626492b
suren-11 <presanth11@gmail.com>
2023-09-29 13:49:07 +0530

    set confirmation alert and checked id already exists

--- js/customer.js
+++ js/customer.js
@@ -1,25 +1,46 @@
-let customers=[];
-function Customer(id,name,address,salary){
-    this.id=id;
-    this.name=name;
-    this.address=address;
-    this.salary=salary;
+let customers = [];
+
+function Customer(id, name, address, salary) {
+    this.id = id;
+    this.name = name;
+    this.address = address;
+    this.salary = salary;
 }
 
-initializeCustomers=()=>{
+initializeCustomers = () => {
     let tempData = JSON.parse(localStorage.getItem('customers'));
-    if (tempData!==null){
-
+    if (tempData !== null) {
+        customers=tempData;
+        console.log(tempData);
     }
 }
 
-function saveCustomer(){
+const launchModel = ()=>{
+    document.getElementById('success-model').click();
+    $('#success-model').click();
+}
+
+function saveCustomer() {
     let customer = new Customer(
         $('#customerId').val(),
         $('#customerName').val(),
         $('#customerAddress').val(),
         Number($('#customerSalary').val())
     );
-    customers.push(customer);
-    localStorage.setItem('customer',JSON.stringify(customers));
+
+    if (customers.find(data=>data.id==customer.id)===undefined){
+        customers.push(customer);
+        localStorage.setItem('customers', JSON.stringify(customers));
+        clearFields();
+        launchModel();
+    }else {
+        alert('Already Exists!');
+    }
+}
+
+const clearFields = () => {
+    $('#customerId').val('');
+    $('#customerName').val('');
+    $('#customerAddress').val('');
+    $('#customerSalary').val('');
 }
