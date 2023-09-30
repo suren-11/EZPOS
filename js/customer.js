ea30e925eab337fa04e4454b1bc157d648544a51
suren-11 <presanth11@gmail.com>
2023-09-30 18:17:16 +0530

    load customer table in customer form

--- js/customer.js
+++ js/customer.js
@@ -10,12 +10,30 @@ function Customer(id, name, address, salary) {
 initializeCustomers = () => {
     let tempData = JSON.parse(localStorage.getItem('customers'));
     if (tempData !== null) {
-        customers=tempData;
+        customers = tempData;
         console.log(tempData);
+        setTableData();
     }
 }
 
-const launchModel = (type,message)=>{
+function setTableData(){
+    let htmlData = '';
+    customers.forEach(data => {
+        htmlData += `<tr>
+    <td>${data.id}</td>
+    <td>${data.name}</td>
+    <td>${data.address}</td>
+    <td>${data.salary}</td>
+    <td>
+    <button class="btn btn-success btn-sm" >Update</button> |
+    <button class="btn btn-danger btn-sm" >Delete</button>
+</td>
+</tr>`;
+    });
+    $('#table-body').html(htmlData);
+}
+
+const launchModel = (type, message) => {
     //document.getElementById('success-model').click();
     $('#exampleModalLabel').html(type);
     $('#modal-body').html(message);
@@ -31,13 +49,13 @@ function saveCustomer() {
         Number($('#customerSalary').val())
     );
 
-    if (customers.find(data=>data.id==customer.id)===undefined){
+    if (customers.find(data => data.id == customer.id) === undefined) {
         customers.push(customer);
         localStorage.setItem('customers', JSON.stringify(customers));
         clearFields();
-        launchModel('success!','Customer Saved');
-    }else {
-        launchModel('warning!','Already exists');
+        launchModel('success!', 'Customer Saved');
+    } else {
+        launchModel('warning!', 'Already exists');
     }
 }
 
