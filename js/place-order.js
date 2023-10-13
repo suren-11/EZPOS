786c48fade05853b679877286510a33c70da8efa
suren-11 <presanth11@gmail.com>
2023-10-13 20:36:32 +0530

    defined the combo box and load the id and codes

--- js/place-order.js
+++ js/place-order.js
@@ -1,3 +1,5 @@
+//<script language="JavaScript" type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+
 let items=[];
 let customers = [];
 
@@ -6,22 +8,45 @@ const loadData = () => {
     let temp = JSON.parse(localStorage.getItem('customers'));
     if (temp !== null) {
         customers = temp;
-        console.log(temp);
         let customerOption = '';
-        customers.forEach(response=>{
-           customerOption+=`<option value="${response.id}" >${response.id}</option>`;
+        customers.forEach(response => {
+            customerOption += `<option value="${response.id}" >${response.id}</option>`;
         });
         $('#customer-id').append(customerOption);
+        loadCustomerData();
     }
 
     let tempData = JSON.parse(localStorage.getItem('items'));
     if (tempData !== null) {
         items = tempData;
-        console.log(tempData);
         let itemOption = '';
-        items.forEach(response=>{
-            itemOption+= `<option value="${response.code}" >${response.code}</option>`
+        items.forEach(response => {
+            itemOption += `<option value="${response.code}" >${response.code}</option>`
         });
         $('#item-code').append(itemOption);
+        loadItemData();
     }
+
+
+    $('#customer-id').change(() => {
+        loadCustomerData();
+    });
+
+    $('#item-code').change(() => {
+        loadItemData();
+    });
+}
+function loadCustomerData(){
+    let temId = $('#customer-id').val();
+    let customer = customers.find(response=>response.id==temId);
+    $('#name').val(customer.name);
+    $('#address').val(customer.address);
+    $('#salary').val(customer.salary);
+}
+function loadItemData(){
+    let tempCode = $('#item-code').val();
+    let item = items.find(response=>response.code==tempCode);
+    $('#description').val(item.description);
+    $('#unit-price').val(item.unitPrice);
+    $('#qty-on-hand').val(item.qtyOnHand);
 }
