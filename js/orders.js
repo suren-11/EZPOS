3ff087a6ad09a3ee9d3c50ffe2c07488509b5df1
suren-11 <presanth11@gmail.com>
2023-10-18 11:37:28 +0530

    defined popup print method and completed all forms

--- js/orders.js
+++ js/orders.js
@@ -19,3 +19,40 @@ function setTableData(){
     });
     $('#table').html(rowData);
 }
+
+const showDetails=(id)=>{
+    const order = orders.find((e)=>id===e.orderId);
+    if(order!==undefined){
+        $('#orderId').html(order.orderId);
+        $('#orderCost').html(order.total);
+        $('#orderDate').html(order.date);
+        $('#customerId').html(order.customer);
+        let customer = JSON.parse(localStorage.getItem('customers'));
+        if(customer!=null){
+            let tempCustomer =customer.find(e=>e.id===order.customer);
+            $('#customerName').html(tempCustomer.name);
+        }
+        let itemRow='';
+        order.orderItems.forEach(response=>{
+            let desc='';
+            let unitPrice='';
+            let item = JSON.parse(localStorage.getItem('items'));
+            if(item!=null){
+                let tempItem =item.find(e=>e.code===response.code);
+                desc = tempItem.description;
+                unitPrice = tempItem.unitPrice;
+            }
+            itemRow+=`<tr>
+<td>${response.code}</td>
+<td>${desc}</td>
+<td>${unitPrice}</td>
+<td>${response.qty}</td>
+<td>${response.total}</td>
+</tr>`;
+        });
+        $('#iTable').html(itemRow);
+        $('#model-button').click();
+
+    }
+}
+
